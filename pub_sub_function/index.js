a4b9d6aa7d01b16c0fa4932974ecb3b30e1b4963
MoritzMohrStade <moritz@mohrstade.de>
2024-02-15 20:11:01 +0100

    added docu

--- pub_sub_function/index.js
+++ pub_sub_function/index.js
@@ -15,49 +15,51 @@ functions.cloudEvent('update_ga4_tables',async cloudEvent => {
 
     const pub_sub_message = base64name
       ? Buffer.from(base64name, 'base64').toString()
-      : 'World';
-  
-    console.log(JSON.parse(pub_sub_message).protoPayload.resourceName);
-    const resource_name = JSON.parse(pub_sub_message).protoPayload.resourceName;
+      : 'empty';
     
-    const ga4 = {
-        project_id: resource_name.split("/")[1],
-        dataset_id: resource_name.split("/")[3],
-        table_id: resource_name.split("/")[5]
-    }
-
-    console.log(ga4);
-    
-    const bigquery = new BigQuery({
-        projectId: ga4.project_id
-    });
-
-    const ga4_options = {
-        clustering: {
-            fields: [cluster_by],
-        },
-    };
+    if(JSON.parse(pub_sub_message).protoPayload.resourceName){
+        console.log(JSON.parse(pub_sub_message).protoPayload.resourceName);
+        const resource_name = JSON.parse(pub_sub_message).protoPayload.resourceName;
         
-    try{
-        await bigquery.dataset(ga4.dataset_id).table(ga4.table_id).setMetadata(ga4_options);
-        console.log(`Table ${ga4.table_id} updated and clustered by: ${cluster_by}.`);
+        const ga4 = {
+            project_id: resource_name.split("/")[1],
+            dataset_id: resource_name.split("/")[3],
+            table_id: resource_name.split("/")[5]
+        }
 
-        const query_update_cluster_rows = `
-            UPDATE \`${ga4.dataset_id}.${ga4.table_id}\`
-            SET stream_id = stream_id
-            WHERE true
-        `;
-    
-        const [job] = await bigquery.createQueryJob({query:query_update_cluster_rows});
-        console.log(`Job ${job.id} started.`);
-
-        // Wait for the query to finish
-        await job.getQueryResults();
-        console.log(`Reprocess for ${ga4.table_id} was successful`);
-    }catch(error){
-        console.log(error.message);
-    }
+        console.log(ga4);
         
-        //res.status(200).send(`Table ${ga4_table_id} updated successfully.`);
-   
+        const bigquery = new BigQuery({
+            projectId: ga4.project_id
+        });
+
+        const ga4_options = {
+            clustering: {
+                fields: [cluster_by],
+            },
+        };
+            
+        try{
+            await bigquery.dataset(ga4.dataset_id).table(ga4.table_id).setMetadata(ga4_options);
+            console.log(`Table ${ga4.table_id} updated and clustered by: ${cluster_by}.`);
+
+            const query_update_cluster_rows = `
+                UPDATE \`${ga4.dataset_id}.${ga4.table_id}\`
+                SET stream_id = stream_id
+                WHERE true
+            `;
+        
+            const [job] = await bigquery.createQueryJob({query:query_update_cluster_rows});
+            console.log(`Job ${job.id} started.`);
+
+            // Wait for the query to finish
+            await job.getQueryResults();
+            console.log(`Reprocess for ${ga4.table_id} was successful`);
+        }catch(error){
+            console.log(error.message);
+        }
+    }
+    else {
+        console.log("protoPayload.resourceName not found.");
+    } 
 });
--- readme.md
+++ readme.md
@@ -1,16 +1,9 @@
 # Cloud Function - Add Clustering to GA4 raw data tables in BigQuery
 
-This cloud function can change the clustering in historical Tables for GA4 to apply clustering. Triggering is done with an http reqeuest
-which contains a post body in following format:
-```javascript
- {   
-    "cluster_by":"stream_id",
-    "project_id": "moritz-test-projekt",
-    "dataset_id":"analytics_262445815_Copy",
-    "start_date": "2024-01-01",
-    "end_date":"2024-01-04"
-} 
-```
+This package contains two different cloud functions to cluster GA4 raw data tables.
+1. HTTP-based: Used to cluster historical GA4 data tables on demand
+2. Pub/Sub-based: Used to automatically cluster GA4 raw data tables when they are getting created
+
 ## Quickstart
 To deploy the function to your GCP project you can do the following:
 1. Download / clone the repository
@@ -19,9 +12,24 @@ To deploy the function to your GCP project you can do the following:
 4. Optional: Change the region for the deployment in the cloudbuild.yaml or / and add a service account if needed
 5. Deploy the cloud function with ```gcloud builds submit```
 
-## Automation
-To run this everytime when a new GA4 table is beeing created you can you use a log filter in connection with Pub/Sub.
+## Http-based
+Uses a post body with the following parameters:
+```javascript
+ {   
+    "cluster_by":"event_name",
+    "project_id": "moritz-test-projekt",
+    "dataset_id":"analytics_262445815_Copy",
+    "start_date": "2024-01-01",
+    "end_date":"2024-02-14"
+} 
+```
+
+## Pub/Sub-based
+This function takes the parameters from the incoming Pub/Sub message from the ``protoPayload.resourceName`` field. You need to set up a log sink with a Pub/Sub topic to use this function correctly. To set the fields to cluster by you need to set the ``cluster_by`` variable in index.js.
+### Automation
+To run this everytime when a new GA4 table is beeing createdmMake sure to use this query for the log sink. This will usually trigger the Cloud Function once per day, when Google pushes new GA4 data to BigQuery.
 ```sql
     proto_payload.resource_name=~"projects/YOUR_PROJECT_ID/datasets/GA4_DATASET_ID/tables/events_2"
     proto_payload.authorization_info.permission="bigquery.tables.create"
 ```
+The standard name for the Pub/Sub topic triggering the Cloud Function is ``ga4_table_created``. You can adjust this in the cloudbuild.yaml inside the pub_sub_function folder. 
